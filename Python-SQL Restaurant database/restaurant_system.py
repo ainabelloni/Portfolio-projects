2ca5b27e06a1517e4e70a0f63cc1939eb7270618
ainabelloni <97523413+ainabelloni@users.noreply.github.com>
2022-01-27 12:34:07 +0100

    Update restaurant_system.py

--- Python-SQL Restaurant database/restaurant_system.py
+++ Python-SQL Restaurant database/restaurant_system.py
@@ -1,3 +1,9 @@
+###### RESTAURANT RESERVATIONS DATABASE USING PYTHON AND SQL######
+
+# The goal of the project is to build a simplified and proof-of-concept software application for managing an electronic table reservation book 
+# for a restaurant in the evening of a specific day. The software is composed of a user interface and business logic part written in Python and 
+# an SQL database management system to store the data.
+
 import mysql.connector
 
 dbconn = mysql.connector.connect(
